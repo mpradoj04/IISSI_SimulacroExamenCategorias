e567a248073c5dd1cf93b04cc18ca02fba0e2fef
Miguel <mprajin860@gmail.com>
2024-05-16 16:47:18 +0200

    Create restaurant Category

--- DeliverUS-Backend/src/controllers/RestaurantCategoryController.js
+++ DeliverUS-Backend/src/controllers/RestaurantCategoryController.js
@@ -1,4 +1,5 @@
 import { RestaurantCategory } from '../models/models.js'
+
 const index = async function (req, res) {
   try {
     const restaurantCategories = await RestaurantCategory.findAll()
@@ -7,7 +8,18 @@ const index = async function (req, res) {
     res.status(500).send(err)
   }
 }
+
+const create = async function (req, res) {
+  const newCategory = RestaurantCategory.build(req.body)
+  try {
+    const category = await newCategory.save()
+    res.json(category)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
 const RestaurantCategoryController = {
-  index
+  index,
+  create
 }
 export default RestaurantCategoryController
--- DeliverUS-Backend/src/routes/RestaurantCategoryRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantCategoryRoutes.js
@@ -1,7 +1,18 @@
 import RestaurantCategoryController from '../controllers/RestaurantCategoryController.js'
+import { isLoggedIn, hasRole } from '../middlewares/AuthMiddleware.js'
+import * as RestaurantCategoryValidation from '../controllers/validation/RestaurantCategoyValidation.js'
+import { handleValidation } from '../middlewares/ValidationHandlingMiddleware.js'
 
 const loadFileRoutes = function (app) {
   app.route('/restaurantCategories')
-    .get(RestaurantCategoryController.index)
+    .get(
+      RestaurantCategoryController.index)
+    .post(
+      isLoggedIn,
+      hasRole('owner'),
+      RestaurantCategoryValidation.create,
+      handleValidation,
+      RestaurantCategoryController.create
+    )
 }
 export default loadFileRoutes
